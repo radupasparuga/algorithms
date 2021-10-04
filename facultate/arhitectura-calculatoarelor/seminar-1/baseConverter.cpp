f31caa7bbf455c33f7b544f4c6e96126c8ef9fb8
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2021-10-05 01:34:07 +0300

    add: bin to hex

--- facultate/arhitectura-calculatoarelor/seminar-1/baseConverter.cpp
+++ facultate/arhitectura-calculatoarelor/seminar-1/baseConverter.cpp
@@ -11,14 +11,14 @@ void nextStep();
 int citireDec();
 int *citireBin();
 void afisareBin(int *bin);
+int *citireHex();
+void afisareHex(int *hex);
 
 // conversii
 int *decToBin(int x);
 int binToDec(int *bin);
-int decToHex(int x);
-int hexToDec(int x);
-int binToHex(int x);
-int hexToBin(int x);
+int *binToHex(int *bin);
+int *hexToBin(int *hex);
 
 int main() {
     chooseConversion();
@@ -27,7 +27,7 @@ int main() {
 
 // Functii meniu
 void chooseConversion() {
-    int choice, dec, *bin;
+    int choice, dec, *bin, *hex;
 
     printf("Introdu codul specific conversiei pe care vrei sa o faci \n");
     printf("Legenda: \n - 1: decimal -> binar \n - 2: binar -> decimal \n - 3: decimal -> hexadecimal \n - 4: hexadecimal -> decimal \n - 5: binar -> hexadecimal \n - 6: hexadecimal -> binar \n - 0: Iesi din program \n");
@@ -47,7 +47,33 @@ void chooseConversion() {
         case 2: // bin to dec
             bin = citireBin();
             dec = binToDec(bin);
-            cout << dec << endl;
+            printf("Afisare in baza 10: %d\n", dec);
+            nextStep();
+            break;
+        case 3: // dec to hex
+            dec = citireDec();
+            bin = decToBin(dec);
+            hex = binToHex(bin);
+            afisareHex(hex);
+            nextStep();
+            break;
+        case 4: // hex to dec
+            hex = citireHex();
+            bin = hexToBin(hex);
+            dec = binToDec(bin);
+            printf("Afisare in baza 10: %d\n", dec);
+            nextStep();
+            break;
+        case 5: // bin to hex
+            bin = citireBin();
+            hex = binToHex(bin);
+            afisareHex(hex);
+            nextStep();
+            break;
+        case 6: // hex to bin
+            hex = citireHex();
+            bin = hexToBin(hex);
+            afisareBin(bin);
             nextStep();
             break;
         default:
@@ -97,19 +123,101 @@ void afisareBin(int *bin) {
     for (int i = 31; i >= 0; --i) {
         if ((i+1) % 8 == 0) printf(" ");
         if (*(bin+i) == 1 && start == 31) start = i;
-        printf("%d", *(bin+i));    
+        printf("%d", *(bin+i));
     }
     printf("\nAfisare scurta: ");
     for (int i = start; i >= 0; --i) printf("%d", *(bin+i));
     printf("\n");
 }
 
+int *citireHex() {
+    static int hex[8];
+    int m = 0;
+    string input;
+
+    cout << "Introdu numarul in format hexadecimal: (litere cu majuscule si fara spatii!) ";
+    cin.ignore();
+    getline(cin, input);
+
+    for (char const &c: input) {
+        if ((int)c >= 65 && (int)c <= 70) {
+            switch((int)c) {
+                case 65:
+                    hex[m++] = 10;
+                    break;
+                case 66:
+                    hex[m++] = 11;
+                    break;
+                case 67:
+                    hex[m++] = 12;
+                    break;
+                case 68:
+                    hex[m++] = 13;
+                    break;
+                case 69:
+                    hex[m++] = 14;
+                    break;
+                case 70:
+                    hex[m++] = 15;
+                    break;
+                default:
+                    printf("Input invalid, incearca din nou.\n");
+                    nextStep();
+                    break;
+            }
+        } else if((int)c >= 48 && (int)c <= 57) {
+            hex[m++] = c - '0';
+        } else {
+            printf("Input invalid, incearca din nou. \n");
+            nextStep();
+        }
+    }
+
+    return hex;
+}
+
+void afisareHex(int *hex) {
+    printf("Afisare hex: ");
+    for (int i = 7; i >= 0; --i) {
+        if ((i+1) % 2 == 0) printf(" ");
+        if (hex[i] > 9) {
+            switch(hex[i]) {
+                case 10:
+                    printf("%s", "A");
+                    break;
+                case 11:
+                    printf("%s", "B");
+                    break;
+                case 12:
+                    printf("%s", "C");
+                    break;
+                case 13:
+                    printf("%s", "D");
+                    break;
+                case 14:
+                    printf("%s", "E");
+                    break;
+                case 15:
+                    printf("%s", "F");
+                    break;
+                default:
+                    printf("%d", hex[i]);
+            }
+        } else {
+            printf("%d", hex[i]);
+        }
+    }
+    printf("\n");
+}
+
 // conversii
 int *decToBin(int x) {
-    static int bin[32];
-    for (int i = 0; x > 0; ++i) {
-        bin[i] = x%2;
-        x = x/2;
+    static int bin[32] = {0};
+    int i = 0;
+    while (x > 0) {
+        bin[i] = x % 2;
+        x = x / 2;
+        i++;
     }
     return bin;
 }
@@ -117,7 +225,40 @@ int *decToBin(int x) {
 int binToDec(int *bin) {
     int result = 0;
     for (int i = 31; i >= 0; --i) {
-        result += *(bin+i) *  pow(2, i);
+        result += *(bin+i) * pow(2, i);
     }
     return result;
+}
+
+int *binToHex(int *bin) {
+    static int hex[8];
+    int binGroup[32] = {0}, m = 7;
+    for (int i = 31; i >= 0; i -= 4) {
+        int dec;
+        binGroup[3] = *(bin+i);
+        binGroup[2] = *(bin+i-1);
+        binGroup[1] = *(bin+i-2);
+        binGroup[0] = *(bin+i-3);
+        dec = binToDec(binGroup);
+        hex[m--] = dec;
+    }
+    return hex;
+}
+
+int *hexToBin(int *hex) {
+    static int bin[32];
+    int m = 31;
+    for (int i = 0; i < 8; ++i) {
+        if (*(hex+i) != 0) {
+            int *auxBin = decToBin(*(hex+i));
+            cout << *(hex+i) << ' ' << *(auxBin+3) << *(auxBin+2) << *(auxBin+1) << *(auxBin) << endl;
+            bin[m--] = *(auxBin+3);
+            bin[m--] = *(auxBin+2);
+            bin[m--] = *(auxBin+1);
+            bin[m--] = *(auxBin);
+        } else {
+            break;
+        }
+    }
+    return bin;
 }
